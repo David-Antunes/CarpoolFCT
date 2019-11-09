2124c55da22514eb9a0f20fc6dee35e575db833a
David-Antunes <51961560+David-Antunes@users.noreply.github.com>
2019-11-09 18:29:55 +0000

    Update Main.java

--- CarpoolFCT/Carpool Project/src/Main.java
+++ CarpoolFCT/Carpool Project/src/Main.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy;
 
 import CarpoolHandler.AlreadyExistsElementException;
 import CarpoolHandler.CarpoolHandler;
 import CarpoolHandler.CarpoolHandlerClass;
+import CarpoolHandler.DateClass;
+import CarpoolHandler.InvalidArgsException;
+import CarpoolHandler.InvalidDateException;
 import CarpoolHandler.InvalidPasswordException;
 import CarpoolHandler.NonExistingElementException;
 
 public class Main {
-	
-	
+
 	// Command Constants
 
 	private static final String AJUDA = "AJUDA";
@@ -31,20 +32,19 @@ public class Main {
 	private static final String COMM_BOLEIA = "boleia - Regista uma boleia para uma dada deslocacao";
 	private static final String COMM_CONSULTA = "consulta - Lista a informacao de uma dada deslocacao";
 	private static final String COMM_RETIRA = "retira - Retira uma dada boleia";
-	private static final String COMM_REMOVE = "remove - Elimina uma dada deslocacao"; 
+	private static final String COMM_REMOVE = "remove - Elimina uma dada deslocacao";
 	private static final String COMM_TERMINA = "termina - Termina a execucao do programa";
 	private static final String COMM_REGISTA = "regista - Regista um novo utilizador no programa";
 	private static final String COMM_ENTRADA = "entrada - Permite a entrada (\"login\") dum utilizador no programa";
 
 	private static final String PROMPT = ">";
-	
+
 	private static final String INV_COMM = "Comando invalido";
 
-	
 	private static final String GOODBYE = "Obrigado. Ate a proxima.";
 	private static final String NAME = "nome (maximo 50 caracteres): ";
 	private static final String PASSWORD = "password (entre 4 e 6 caracteres - digitos e letras): ";
-	private static final String NO_REGIST = "Registo nao efetuado" ;
+	private static final String NO_REGIST = "Registo nao efetuado";
 	private static final String USER_EXISTS = "Utilizador ja existente.";
 	private static final String N_USERS = "Registo %d efetuado\n";
 	private static final String NO_USER_EXISTS = "Utilizador nao existente.";
@@ -52,266 +52,275 @@ public class Main {
 	private static final String VISIT_NUM = "Visita %d efetuada\n";
 
 	public static void main(String[] args) {
-		
 
 		Scanner in = new Scanner(System.in);
 		CarpoolHandler ch = new CarpoolHandlerClass();
 		String option = readOption(in, ch);
-		do { 
+		do {
 			executeOption(option, in, ch);
 			option = readOption(in, ch);
-		} while (!option.equals(TERMINA) || ch.hasCurUsar());
+		} while (!option.equals(TERMINA) || ch.hasCurrUser());
 		System.out.println(GOODBYE);
 		in.close();
 
 	}
 
-		// Reads the command that we want to execute
+	// Reads the command that we want to execute
 
-		private static String readOption(Scanner in, CarpoolHandler ch) {
-			printPrompt(ch);
-			return in.next().toUpperCase();
+	private static String readOption(Scanner in, CarpoolHandler ch) {
+		printPrompt(ch);
+		return in.next().toUpperCase();
+	}
+
+	/**
+	 * Prints prompt whit email if session is initiated and without it if the
+	 * session is not
+	 */
+
+	private static void printPrompt(CarpoolHandler ch) {
+
+		try {
+			System.out.print(ch.userEmail() + " " + PROMPT);
+		} catch (NonExistingElementException e) {
+			System.out.print(PROMPT);
 		}
+	}
 
-		/**
-		 * Prints prompt whit email if session is initiated and without it if the session is not
-		 */
+	/**
+	 * Executes the option with two different ways depending if the session is
+	 * initiated or not
+	 */
 
-		private static void printPrompt(CarpoolHandler ch) {
-			
-			try {
-				System.out.print(ch.userEmail() + " " + PROMPT); 
-			}
-			catch(NonExistingElementException e) {
-				System.out.print(PROMPT);
-			}
-		}
+	private static void executeOption(String option, Scanner in, CarpoolHandler ch) {
+		if (ch.hasCurrUser())
+			executeOptionSessionMode(option, in, ch);
+		else
+			executeOptionInicialMode(option, in, ch);
+	}
 
-		/**
-		 * Executes the option with two different ways depending if the session is initiated or not
-		 */
+	/**
+	 * Executes commands when session is not initiated Commands are AJUDA TERMINA
+	 * REGISTA ENTRADA
+	 */
 
-		private static void executeOption(String option, Scanner in, CarpoolHandler ch) {
-			if (ch.hasCurUsar())  
-				executeOptionSessionMode(option, in, ch);
-			else
-				executeOptionInicialMode(option, in, ch);
-		}
+	private static void executeOptionInicialMode(String option, Scanner in, CarpoolHandler ch) {
+		switch (option) {
 
-		/**
-		 * Executes commands when session is not initiated 
-		 * Commands are
-		 * AJUDA TERMINA REGISTA ENTRADA
-		 */
+		case AJUDA:
+			ajuda(in, ch);
+			break;
 
-		private static void executeOptionInicialMode(String option, Scanner in, CarpoolHandler ch) {
-			switch (option) {
+		case TERMINA:
+			terminar();
+			break;
 
-			case AJUDA:
-				ajuda(in, ch);
-				break;
+		case REGISTA:
+			regista(in, ch);
+			break;
 
-			case TERMINA:
-				terminar();
-				break;
-
-			case REGISTA:
-				regista(in, ch);
-				break;
-
-			case ENTRADA:
-				entrada(in, ch);
-				break;
-			default:
-				System.out.println(INV_COMM);
-				in.nextLine();
-				break;
-
-			}
-
-		}
-
-
-		/**
-		 * Executes commands when session is not initiated 
-		 * Commands are
-		 * SAI NOVA LISTA BOLEIA CONSULTA REMOVE
-		 */
-
-		private static void executeOptionSessionMode(String option, Scanner in, CarpoolHandler ch) {
-			switch (option) {
-
-			case AJUDA:
-				ajuda(in, ch);
-				break;
-
-			case SAI:
-				sai(ch);
-				break;
-
-			case NOVA:
-				nova(in, ch);
-				break;
-
-			case LISTA:
-				lista(in, ch);
-				break;
-
-			case BOLEIA:
-				boleia(in, ch);
-				break;
-
-			case CONSULTA:
-				consulta(in, ch);
-				break;
-
-			case REMOVE:
-				remove(in, ch);
-				break;
-
-			default:
-				System.out.println(INV_COMM);
-				in.nextLine();
-				break;
-
-			}
-		}
-		
-		private static void entrada(Scanner in, CarpoolHandler ch) {
-			boolean passCorrect = false;
-			String password = null;
-			String  email = in.next();
+		case ENTRADA:
+			entrada(in, ch);
+			break;
+		default:
+			System.out.println(INV_COMM);
 			in.nextLine();
-			try {
-				ch.userExists(email);
-				for (int i = 1;!passCorrect && i <= 3; i++) { 
-					System.out.print(PASSWORD);
-					password = in.nextLine();
-					if(ch.isPassCorrect(email,password, i))
-						passCorrect = true;
-				}
-				
-				ch.login(email);
-				System.out.printf(VISIT_NUM, ch.nVisitas());
-		
-				
-			}
-			catch (NonExistingElementException e){
-				System.out.println(NO_USER_EXISTS);
-			} 
-			
-			catch (InvalidPasswordException e) {
-				System.out.println(NO_ENTRY);
-			}
-			
-		}
-
-		private static void remove(Scanner in, CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		private static void consulta(Scanner in, CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		private static void boleia(Scanner in, CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		private static void lista(Scanner in, CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		private static void nova(Scanner in, CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		private static void sai(CarpoolHandler ch) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		/**
-		 * Command AJUDA
-		 * Prints menus when the session is and is not initiated
-		 */
-
-		private static void ajuda(Scanner in, CarpoolHandler ch) {
-			System.out.println(COMM_AJUDA);
-
-			if (!ch.hasCurUsar()) { // arranjar metodo ou assim para "dividir"
-				System.out.println(COMM_SAI);
-				System.out.println(COMM_NOVA);
-				System.out.println(COMM_LISTA);
-				System.out.println(COMM_BOLEIA);
-				System.out.println(COMM_CONSULTA);
-				System.out.println(COMM_RETIRA);
-				System.out.println(COMM_REMOVE);
-			} else {
-				System.out.println(COMM_TERMINA);
-				System.out.println(COMM_REGISTA);
-				System.out.println(COMM_ENTRADA);
-
-			}
-		}
-
-		/**
-		 * Command TERMINA
-		 * Pints goodbye message when the program stops executing
-		 */
-
-		private static void terminar() {
-
-			System.out.println(GOODBYE);
+			break;
 
 		}
-		
 
-		/**
-		 * Command REGISTA
-		 * register new user
-		 * @throws NonExistingElementException 
-		 */
+	}
 
-		private static void regista(Scanner in, CarpoolHandler ch)  {
-			boolean passValid = false;
-			String name;
-			String password = null;
-			String email = in.next();
+	/**
+	 * Executes commands when session is not initiated Commands are SAI NOVA LISTA
+	 * BOLEIA CONSULTA REMOVE
+	 */
+
+	private static void executeOptionSessionMode(String option, Scanner in, CarpoolHandler ch) {
+		switch (option) {
+
+		case AJUDA:
+			ajuda(in, ch);
+			break;
+
+		case SAI:
+			sai(ch);
+			break;
+
+		case NOVA:
+			nova(in, ch);
+			break;
+
+		case LISTA:
+			lista(in, ch);
+			break;
+
+		case BOLEIA:
+			boleia(in, ch);
+			break;
+
+		case CONSULTA:
+			consulta(in, ch);
+			break;
+
+		case REMOVE:
+			remove(in, ch);
+			break;
+
+		default:
+			System.out.println(INV_COMM);
 			in.nextLine();
-			
-			
-			try {
-				
-				ch.hasUser(email);
-				System.out.print(NAME);
-				name = in.nextLine();
-				for (int i = 1;!passValid && i <= 3; i++) { 
-					System.out.print(PASSWORD);
-					password = in.nextLine();
-					if(ch.validPassaword(password, i))
-						passValid = true;
-						}
-				
-					ch.register(email, name, password);
-					System.out.printf(N_USERS, ch.nUsers());
-					
-					
-			
-				}
-				
-				catch (AlreadyExistsElementException e){
-					System.out.println(USER_EXISTS); 
-				}
-				
-				catch (InvalidPasswordException e) {
-					System.out.println(NO_REGIST);
-				}
-}	
-}		
+			break;
 
+		}
+	}
+
+	private static void entrada(Scanner in, CarpoolHandler ch) {
+		boolean passCorrect = false;
+		String password = null;
+		String email = in.next();
+		in.nextLine();
+		try {
+			ch.userExists(email);
+			for (int i = 1; !passCorrect && i <= 3; i++) {
+				System.out.print(PASSWORD);
+				password = in.nextLine();
+				if (ch.isPassCorrect(email, password, i))
+					passCorrect = true;
+			}
+
+			ch.login(email);
+			System.out.printf(VISIT_NUM, ch.nVisitas());
+
+		} catch (NonExistingElementException e) {
+			System.out.println(NO_USER_EXISTS);
+		}
+
+		catch (InvalidPasswordException e) {
+			System.out.println(NO_ENTRY);
+		}
+
+	}
+
+	private static void remove(Scanner in, CarpoolHandler ch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void consulta(Scanner in, CarpoolHandler ch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void boleia(Scanner in, CarpoolHandler ch) {
+
+	}
+
+	private static void lista(Scanner in, CarpoolHandler ch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void nova(Scanner in, CarpoolHandler ch) {
+		try {
+
+			String origin = in.nextLine();
+			String destiny = in.nextLine();
+			String line = in.nextLine();
+			String[] split = line.split(" ");
+			String date = split[0];
+
+			String[] time = split[1].split(":");
+			int hour = Integer.parseInt(time[0]);
+			int minutes = Integer.parseInt(time[1]);
+			int duration = Integer.parseInt(split[2]);
+			int seats = Integer.parseInt(split[3]);
+
+			ch.Ride(origin, destiny, new DateClass(date), hour, minutes, duration, seats);
+
+		} catch (NumberFormatException e) {
+			System.out.println("Dados Invalidos.");
+		} catch (InvalidArgsException e) {
+			System.out.println("Dados Invalidos.");
+		} catch (InvalidDateException e) {
+			System.out.println(ch.getCurrUser().getName() + " ja tem uma deslocacao ou boleia registada nesta data.");
+		}
+
+	}
+
+	private static void sai(CarpoolHandler ch) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Command AJUDA Prints menus when the session is and is not initiated
+	 */
+
+	private static void ajuda(Scanner in, CarpoolHandler ch) {
+		System.out.println(COMM_AJUDA);
+
+		if (!ch.hasCurrUser()) { // arranjar metodo ou assim para "dividir"
+			System.out.println(COMM_SAI);
+			System.out.println(COMM_NOVA);
+			System.out.println(COMM_LISTA);
+			System.out.println(COMM_BOLEIA);
+			System.out.println(COMM_CONSULTA);
+			System.out.println(COMM_RETIRA);
+			System.out.println(COMM_REMOVE);
+		} else {
+			System.out.println(COMM_TERMINA);
+			System.out.println(COMM_REGISTA);
+			System.out.println(COMM_ENTRADA);
+
+		}
+	}
+
+	/**
+	 * Command TERMINA Pints goodbye message when the program stops executing
+	 */
+
+	private static void terminar() {
+
+		System.out.println(GOODBYE);
+
+	}
+
+	/**
+	 * Command REGISTA register new user
+	 * 
+	 * @throws NonExistingElementException
+	 */
+
+	private static void regista(Scanner in, CarpoolHandler ch) {
+		boolean passValid = false;
+		String name;
+		String password = null;
+		String email = in.next();
+		in.nextLine();
+
+		try {
+
+			ch.hasUser(email);
+			System.out.print(NAME);
+			name = in.nextLine();
+			for (int i = 1; !passValid && i <= 3; i++) {
+				System.out.print(PASSWORD);
+				password = in.nextLine();
+				if (ch.validPassaword(password, i))
+					passValid = true;
+			}
+
+			ch.register(email, name, password);
+			System.out.printf(N_USERS, ch.nUsers());
+
+		}
+
+		catch (AlreadyExistsElementException e) {
+			System.out.println(USER_EXISTS);
+		}
+
+		catch (InvalidPasswordException e) {
+			System.out.println(NO_REGIST);
+		}
+	}
+}
