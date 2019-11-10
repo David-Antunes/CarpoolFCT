fbc5ae1f3e2a94be1752a6961375186bdccb45a8
carolina-duarte <56353649+carolina-duarte@users.noreply.github.com>
2019-11-10 23:34:25 +0000

    Add files via upload
    comentarios nas interfaces falta  main

--- Date.java
+++ Date.java
@@ -2,13 +2,34 @@ package CarpoolHandler;
 
 public interface Date{
 	
-	int getDay();
+	/**
+	 * Returns the day of the date
+	 * @return day
+	 */
+	String getDay();
 	
-	int getMonth();
+	/**
+	 * Returns the month of the date
+	 * @return month
+	 */
+	String getMonth();
 	
-	int getYear();
+	/**
+	 * Returns the year of the date
+	 * @return year
+	 */
+	String getYear();
 	
+	/**
+	 * Returns the full date (day+month+year) of the date
+	 * @return full date
+	 */
 	String getFullDate();
 	
+	/**
+	 * Checks if the date is valid
+	 * @param date date
+	 * @return <code>true</code> if is or <code>false</code> if is invalid
+	 */
 	boolean isDateValid(String date);
 }
